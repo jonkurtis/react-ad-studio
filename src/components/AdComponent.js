6457d72a8da169e52d2d354882d81b3d6ffaea68
jonkurtis <jonathankurtis@gmail.com>
2019-04-23 22:39:28 -0400

    Added formik and formik-material-ui packages. Still need to implement. Worked on AdComponent.

--- src/components/AdComponent.js
+++ src/components/AdComponent.js
@@ -5,8 +5,24 @@ const AdComponent = props => {
   const { form } = props;
 
   return (
-    <div style={{marginTop: '50px'}}>
-      <button>{form.CTA}</button>
+    <div style={{
+      margin: 'auto',
+      marginTop: '50px',
+      width: '300px',
+      height: '250px',
+      position: 'relative',
+      border: '1px solid white',
+      }}>
+      <h1>2019 Honda Civic LX</h1>
+      <button style={{
+        borderRadius: '4px',
+        background: 'green',
+        color: 'white',
+        border: 'none',
+        padding: '10px 32px',
+        fontSize: '26px',
+        margin: 'auto'
+      }}>{form.CTA}</button>
     </div>
   )
 }
